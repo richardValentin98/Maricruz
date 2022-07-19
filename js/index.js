86285df2fca5bbcceb9f660291cc07872fd03b7f
richardValentin98 <86756503+richardValentin98@users.noreply.github.com>
2022-07-19 00:25:54 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('💞 LLEGASTE DE UNA FORMA ESPECIAL!!!! MARYJEY 💞');
+    alert('😱 ERES LA MÁS HERMOSA POR ESO TE MANDO ESTO!😱');
+    location.href = 'https://www.youtube.com/watch?v=13jQjFQmTFw&ab_channel=BlueCard';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  const partner = prompt('🥰 DIME TU NOMBRE 🥰');
+  heroTitle.innerHTML += partner + ' ❤';
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
