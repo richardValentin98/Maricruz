bc4c6e2ccaa456a8666c83974069c2b960b7e40e
richardValentin98 <86756503+richardValentin98@users.noreply.github.com>
2022-08-23 22:13:52 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('LLEGASTE DE UNA FORMA ESPECIAL!!!! JASMIN ');
+    alert('😱 ERES LA MÁS HERMOSA POR ESO TE MANDO ESTO!😱');
+    location.href = 'https://www.youtube.com/watch?v=zkPCe5FHbDI';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  const partner = prompt('🥰 DIME TU NOMBRE 🥰');
+  heroTitle.innerHTML += partner + ' ❤';
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
